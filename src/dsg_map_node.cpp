7da700bac2a4214b5962d692d575a44be012629d
Joao Lucas <joaolucasmbcadorniga@gmail.com>
2025-07-22 10:39:11 -0500

    feat: new service to post nav2 goal for an object

--- src/dsg_map_node.cpp
+++ src/dsg_map_node.cpp
@@ -1,4 +1,5 @@
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
 #include "hydra_msgs/msg/dsg_update.hpp"
 #include "spark_dsg/serialization/graph_binary_serialization.h"
 #include "spark_dsg/dynamic_scene_graph.h"
@@ -8,6 +9,7 @@
 #include "std_srvs/srv/empty.hpp"
 #include "dsg_query/srv/list_rooms.hpp"
 #include "dsg_query/srv/list_room_objects.hpp"
+#include "dsg_query/srv/move_to_object.hpp"
 #include "dsg_query/msg/room.hpp"
 #include "dsg_query/msg/room_object.hpp"
 
@@ -16,6 +18,7 @@
 #include <pcl/point_types.h>
 #include <pcl/io/ply_io.h>
 #include <pcl/features/from_meshes.h>
+#include <pcl/common/io.h>
 
 #include "dsg_query/pcl_mesh_to_assimp.h"
 #include <assimp/scene.h>
@@ -24,12 +27,20 @@
 #include <assimp/material.h>
 
 #include <cstdio>
-#include <filesystem> 
+#include <filesystem>
+#include <chrono>
+
+#include "nav2_msgs/action/navigate_to_pose.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+
+using NavigateToPose = nav2_msgs::action::NavigateToPose;
+using GoalHandleNav  = rclcpp_action::ClientGoalHandle<NavigateToPose>;
 
 using Room = dsg_query::msg::Room;
 using RoomObject = dsg_query::msg::RoomObject;
 using ListRooms = dsg_query::srv::ListRooms;
 using ListRoomObjects = dsg_query::srv::ListRoomObjects;
+using MoveToObject = dsg_query::srv::MoveToObject;
 
 class DsgMapServer : public rclcpp::Node {
 public:
@@ -59,6 +70,14 @@ public:
     list_objects_service_ = this->create_service<ListRoomObjects>(
       "/hydra/backend/objects",
       std::bind(&DsgMapServer::listObjectsCallback, this, std::placeholders::_1, std::placeholders::_2));
+
+    nav_to_pose_client_ = rclcpp_action::create_client<NavigateToPose>(
+      this, "navigate_to_pose");
+
+    // Create a service to move to an object, receives a MoveToObject request
+    move_to_object_service_ = this->create_service<MoveToObject>(
+      "/hydra/backend/moveToObject",
+      std::bind(&DsgMapServer::moveToObjectCallback, this, std::placeholders::_1, std::placeholders::_2));
   }
       
 private:
@@ -203,12 +222,99 @@ private:
       auto& room_node = graph_->getNode(*room);
 
       if (room_node.id == request->room.room_id) {
+        const auto attrs = node.attributes<spark_dsg::SemanticNodeAttributes>();
+
         RoomObject object_msg;
         object_msg.object_id = node.id;
+        object_msg.semantic_label = attrs.semantic_label;
         object_msg.room_id = room_node.id;
+        object_msg.x = attrs.position.x();
+        object_msg.y = attrs.position.y();
+        object_msg.z = attrs.position.z();
         response->objects.push_back(object_msg);
       }
     }
+  }
+
+  void sendGoal(const geometry_msgs::msg::PoseStamped & goal_pose)
+  {
+    // Make sure the Nav2 action server is up
+    if (!nav_to_pose_client_->wait_for_action_server(std::chrono::seconds(5))) {
+      RCLCPP_ERROR(get_logger(), "NavigateToPose action server not available");
+      return;
+    }
+
+    // Fill in the goal message
+    NavigateToPose::Goal goal_msg;
+    goal_msg.pose = goal_pose;
+
+    // Optional: set up callbacks for feedback/result
+    auto send_opts = rclcpp_action::Client<NavigateToPose>::SendGoalOptions();
+    // send_opts.goal_response_callback =
+    //   [](GoalHandle::SharedPtr goal_handle) {
+    //     if (!goal_handle) {
+    //       RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),
+    //                   "NavigateToPose goal was rejected by server");
+    //     } else {
+    //       RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
+    //                   "NavigateToPose goal accepted, waiting for result");
+    //     }
+    //   };
+    send_opts.feedback_callback =
+      [](GoalHandleNav::SharedPtr,
+         const std::shared_ptr<const NavigateToPose::Feedback> feedback) {
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
+                    "Distance remaining: %.2f",
+                    feedback->distance_remaining);
+      };
+    send_opts.result_callback =
+      [](const GoalHandleNav::WrappedResult & result) {
+        switch (result.code) {
+          case rclcpp_action::ResultCode::SUCCEEDED:
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
+                        "Navigation succeeded!");
+            break;
+          case rclcpp_action::ResultCode::ABORTED:
+            RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),
+                         "Navigation was aborted");
+            break;
+          case rclcpp_action::ResultCode::CANCELED:
+            RCLCPP_WARN(rclcpp::get_logger("rclcpp"),
+                        "Navigation was canceled");
+            break;
+          default:
+            RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),
+                         "Unknown result code");
+            break;
+        }
+      };
+
+    // Finally send the goal
+    nav_to_pose_client_->async_send_goal(goal_msg, send_opts);
+  }
+
+  void moveToObjectCallback(
+      const std::shared_ptr<MoveToObject::Request> request,
+      std::shared_ptr<MoveToObject::Response>) {
+  
+    if (!graph_->hasNode(request->object.object_id)) {
+      RCLCPP_ERROR(this->get_logger(), "Object with ID %lu not found", request->object.object_id);
+      return;
+    }
+      
+    // Find the object in the DSG
+    auto objects = graph_->findLayer("OBJECTS");
+    auto& object_node = objects->getNode(request->object.object_id);
+
+    const auto attrs = object_node.attributes<spark_dsg::SemanticNodeAttributes>();
+    geometry_msgs::msg::PoseStamped goal_pose;
+    goal_pose.header.frame_id = "map";
+    goal_pose.pose.position.x = attrs.position.x();
+    goal_pose.pose.position.y = attrs.position.y();
+    goal_pose.pose.position.z = attrs.position.z();
+    goal_pose.pose.orientation.w = 1.0;
+
+    sendGoal(goal_pose);
   }
 
   std::shared_ptr<spark_dsg::DynamicSceneGraph> graph_;
@@ -217,6 +323,9 @@ private:
   rclcpp::Service<std_srvs::srv::Empty>::SharedPtr save_dsg_service_;
   rclcpp::Service<ListRooms>::SharedPtr list_rooms_service_;
   rclcpp::Service<ListRoomObjects>::SharedPtr list_objects_service_;
+  rclcpp::Service<MoveToObject>::SharedPtr move_to_object_service_;
+  rclcpp_action::Client<NavigateToPose>::SharedPtr nav_to_pose_client_;
+
 };
 
 int main(int argc, char** argv) {
